513963c034714ec21253b82467ce1c185435f97b
Tim <gerald.timtiman@acacia-soft.com>
2020-04-13 20:57:21 +0800

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url(auth()->user()->role == 'admin' ? 'home-admin' : 'home-user') }}">Home</a>
+                        <a href="{{ url(auth()->user()->hasPermissionTo('view admin') ? 'home-admin' : 'home-user') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
